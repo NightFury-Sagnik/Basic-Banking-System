28d7edca7ad7424ccc7ca8cf3b45b59744326b1c
Sagnik Chatterjee <chatterjeesagnik99@gmail.com>
2021-10-14 19:23:18 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+	$conn = mysqli_connect('localhost','root','','sparks');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
+<!--'shareddb-x.hosting.stackcp.net','shivangi','12345678@','malyaraaj-313537e38d'-->
